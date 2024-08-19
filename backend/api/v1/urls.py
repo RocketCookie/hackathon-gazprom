a29d1ba5f2aa2d5456b38be47ad34bea29afedbe
Oleg Gurov <guroff.oleg@gmail.com>
2024-08-19 08:08:17 +0300

    Добавлены API

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -1,6 +1,20 @@
 from django.urls import include, path
 
+from api.v1.views import (
+    PlatformListView,
+    ProductListView,
+    ServiceListView,
+    TeamListView,
+)
+
 urlpatterns = [
+    path("platforms/", PlatformListView.as_view(), name="platform-list"),
+    path("products/", ProductListView.as_view(), name="product-list"),
+    path("services/", ServiceListView.as_view(), name="service-list"),
+    path("teams/", TeamListView.as_view(), name="team-list"),
+]
+
+urlpatterns += [
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.authtoken")),
 ]
--- backend/api/v1/views.py
+++ backend/api/v1/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
 
-# Create your views here.
+from api.v1.serializers import (
+    PlatformSerializer,
+    ProductSerializer,
+    ServiceSerializer,
+    TeamSerializer,
+)
+from leadership.models import LeaderPlatform, LeaderProduct, LeaderService
+from platforms.models import Platform, Product, Service
+from teams.models import Team
+
+
+class PlatformListView(ListAPIView):
+    queryset = Platform.objects.all()
+    serializer_class = PlatformSerializer
+
+
+class ProductListView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ServiceListView(ListAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+
+
+class TeamListView(ListAPIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
